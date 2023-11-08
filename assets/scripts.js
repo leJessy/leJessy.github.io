9ce374245bc67b70d427589badc03ef1a5c3ca77
Jessy <ayy.jessy@gmail.com>
2023-11-08 14:36:43 +0100

    Fix portfolio

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,43 +1,34 @@
-// document.addEventListener('DOMContentLoaded', function () {
-//    const projects = document.querySelectorAll('.portfolio-item');
-
-//    projects.forEach(project => {
-//        const modal = project.querySelector('.modal');
-
-//        project.addEventListener('mouseenter', function () {
-//            modal.style.display = 'flex';
-//        });
-
-//        project.addEventListener('mouseleave', function () {
-//            modal.style.display = 'none';
-//        });
-//    });
-// });
-
 document.addEventListener('DOMContentLoaded', function () {
     const projects = document.querySelectorAll('.portfolio-item');
 
     projects.forEach(project => {
         const modal = project.querySelector('.modal');
-
-        // Utilisation de MutationObserver pour détecter les changements dans l'arbre DOM
-        const observer = new MutationObserver(function (mutationsList, observer) {
-            // Gérer les mutations ici si nécessaire
-        });
-
-        // Configuration de l'observateur pour surveiller les changements dans les attributs et les enfants
-        const config = { attributes: true, childList: true, subtree: true };
-
-        // Commencer à observer l'élément modal
-        observer.observe(modal, config);
+        let isModalOpen = false;
 
         project.addEventListener('mouseenter', function () {
             modal.style.display = 'flex';
+            isModalOpen = true;
         });
 
         project.addEventListener('mouseleave', function () {
-            modal.style.display = 'none';
+            if (isModalOpen) {
+                modal.style.display = 'none';
+                isModalOpen = false;
+            }
+        });
+
+        project.addEventListener('click', function () {
+            if (!isModalOpen) {
+                modal.style.display = 'flex';
+                isModalOpen = true;
+            } else {
+                modal.style.display = 'none';
+                isModalOpen = false;
+            }
+        });
+        
+        modal.addEventListener('touchstart', function (event) {
+            event.stopPropagation();
         });
     });
 });
-
